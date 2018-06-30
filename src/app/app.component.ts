c5cb6a64a8245520ebc1503ff198b3dae7bf6501
Farzad Rahman <farzadrahman59@gmail.com>
2018-06-30 15:01:50 +0600

    added login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {TokenService} from "./services/token.service";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(private token:TokenService) {
+  }
+
+  isLogIn(){
+
+    var url=this.token.getUrl();
+
+    if(url=='/login'){
+      return false;
+    }
+    else {
+      return true;
+    }
+
+  }
+
 }
