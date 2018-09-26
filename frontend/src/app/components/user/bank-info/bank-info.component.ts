f6cd9fc0c3b8dada6beba9fb49c4c6aba5d0833f
Farzad Rahman <farzadrahman59@gmail.com>
2018-09-26 17:33:54 +0600

    Farzad

--- frontend/src/app/components/user/bank-info/bank-info.component.ts
+++ frontend/src/app/components/user/bank-info/bank-info.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit, Input} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {TokenService} from "../../../services/token.service";
+import {Router} from "@angular/router";
+import {Constants} from "../../../constants";
 
 @Component({
   selector: 'app-bank-info',
@@ -8,9 +12,46 @@ import {Component, OnInit, Input} from '@angular/core';
 export class BankInfoComponent implements OnInit {
 
   @Input('empid') empid: any;
-  constructor() { }
+  employeeBankForm:any={
+    id:'',
+    pfAccountNo:'',
+    bankAccountNo:'',
+    tinId:''
+  };
+  result:any;
+  constructor(public http: HttpClient, private token:TokenService,private router: Router) { }
 
   ngOnInit() {
+    this.employeeBankForm.id=this.empid;
+
+  }
+
+  submit(){
+    const token=this.token.get();
+    this.http.post(Constants.API_URL+'bankinfo/post'+'?token='+token,this.employeeBankForm).subscribe(data => {
+          this.result=data;
+          $.alert({
+            title: 'Success!',
+            type: 'Green',
+            content: this.result.message,
+            buttons: {
+              tryAgain: {
+                text: 'Ok',
+                btnClass: 'btn-red',
+                action: function () {
+                }
+              }
+            }
+          });
+
+        },
+        error => {
+          console.log(error);
+        }
+    );
+
+
+
   }
 
 }
