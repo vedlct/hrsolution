b127cf95f5f15775f0968afd63b8a2f82ae53983
nsmajm <nsmajm@gmail.com>
2018-10-22 12:14:57 +0600

    nasim

--- backend/app/Http/Controllers/EmergencyContactController.php
+++ backend/app/Http/Controllers/EmergencyContactController.php
@@ -34,7 +34,7 @@ class EmergencyContactController extends Controller
 
     }
     public function getEmergencyContact(Request $r){
-        $emergencyContact = EmergencyContact::all()->where('EmergencyContact','=',$r->id)->first();
+        $emergencyContact = EmergencyContact::where('EmergencyContact','=',$r->id)->first();
         return response()->json($emergencyContact);
     }
 }
