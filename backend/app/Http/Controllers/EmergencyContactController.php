5f4c9ae63ce721819bb361e6553f0c80814a6b85
nsmajm <nsmajm@gmail.com>
2018-10-22 13:23:57 +0600

    nasim

--- backend/app/Http/Controllers/EmergencyContactController.php
+++ backend/app/Http/Controllers/EmergencyContactController.php
@@ -17,21 +17,35 @@ class EmergencyContactController extends Controller
             'alterContactNo' => 'nullable|max:15',
             'relation' => 'nullable|max:200',
         ]);
-        if($r->id){
-           $contact = EmergencyContact::findOrfail($r->id);
-        }
+
+           $contact = EmergencyContact::where('fkemployeeId','=',$r->fkemployeeId)->count();
+           if($contact==0){
+
+               $contact = new EmergencyContact();
+               $contact->fkemployeeId = $r->fkemployeeId ;
+               $contact->firstName = $r->firstName ;
+               $contact-> middleName= $r-> middleName;
+               $contact-> lastName= $r-> lastName;
+               $contact->address = $r-> address;
+               $contact->contactNo = $r->contactNo ;
+               $contact->alterContactNo = $r->alterContactNo;
+               $contact->relation = $r-> relation;
+               $contact->save();
+               return response()->json($contact);
+
+           }
+
         else{
-        $contact = new EmergencyContact();
-        $contact->fkemployeeId = $r->fkemployeeId ;
-        $contact->firstName = $r->firstName ;
-        $contact-> middleName= $r-> middleName;
-        $contact-> lastName= $r-> lastName;
-        $contact->address = $r-> address;
-        $contact->contactNo = $r->contactNo ;
-        $contact->alterContactNo = $r->alterContactNo;
-        $contact->relation = $r-> relation;
-        $contact->save();
-        return response()->json($contact);
+            EmergencyContact::where('fkemployeeId',     '=',$r->fkemployeeId)->update([
+                "firstName" => $r->firstName,
+                "middleName"=> $r-> middleN,
+                "lastName"=> $r-> lastName,
+                "address" => $r-> address,
+                "contactNo" => $r->contactNo,
+                "alterContactNo" => $r->alterContactNo,
+                "relation" => $r-> relation
+            ]);
+            return response()->json("Emergency Contact Updated");
         }
 
     }
