7b9c875ec5fd1295209eb2ace6ddeabb8d5295ec
nsmajm <nsmajm@gmail.com>
2018-10-22 11:52:04 +0600

    nasim

---
+++ backend/app/EmergencyContact.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmergencyContact extends Model
+{
+    public $timestamps = false;
+    protected $primaryKey = 'emergency_contact';
+    protected $table = 'emergency_contact';
+}
---
+++ backend/app/Http/Controllers/EmergencyContactController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\EmergencyContact;
+use Illuminate\Http\Request;
+
+class EmergencyContactController extends Controller
+{
+    public function createEmergencyContact(Request $r){
+        $this->validate($r,[
+            'firstName' => 'nullable|max:50',
+            'middleName' => 'nullable|max:50',
+            'address' => 'nullable|max:50',
+            'contactNo' => 'nullable|max:15',
+            'alterContactNo' => 'nullable|max:15',
+            'relation' => 'nullable|max:200',
+        ]);
+        if($r->id){
+           $contact = EmergencyContact::findOrfail($r->id);
+        }
+        else{
+        $contact = new EmergencyContact();
+        $contact->fkemployeeId = $r->id ;
+        $contact->firstName = $r->firstName ;
+        $contact-> middleName= $r-> middleName;
+        $contact->address = $r-> address;
+        $contact->contactNo = $r->contactNo ;
+        $contact->alterContactNo = $r->alterContactNo ;
+        $contact->relation = $r-> relation;
+        $contact->save();
+        return response()->json($contact);
+        }
+
+    }
+}
