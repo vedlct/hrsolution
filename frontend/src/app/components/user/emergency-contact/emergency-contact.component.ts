2305a9c6bce203761ec5aba5ca698da8ee065406
Farzad Rahman <farzadrahman59@gmail.com>
2018-10-22 12:08:27 +0600

    Farzad

--- frontend/src/app/components/user/emergency-contact/emergency-contact.component.ts
+++ frontend/src/app/components/user/emergency-contact/emergency-contact.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {Router} from "@angular/router";
+import {TokenService} from "../../../services/token.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-emergency-contact',
@@ -6,20 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./emergency-contact.component.css']
 })
 export class EmergencyContactComponent implements OnInit {
+  @Input('empid') empid: any;
+  error=[];
   emergencyForm:any={
     id:'',
     firstName:'',
     middleName:'',
     lastName:'',
-    email:'',
     contactNo:'',
     alterContactNo:'',
+    address:''
 
   };
 
-  constructor() { }
+  constructor(public http: HttpClient, private token:TokenService,private router: Router) { }
 
   ngOnInit() {
+    this.emergencyForm.id=this.empid;
+    // this.emergencyForm.address=this.empid;
+
+  }
+
+  submitForm(){
+    // const data=error.error.errors;
+    //
+    // for (var p in data) {
+    //
+    //   for (var k in data[p]) {
+    //     this.error.push(data[p][k]);
+    //   }
+    // }
+    console.log(this.emergencyForm);
   }
 
 }
