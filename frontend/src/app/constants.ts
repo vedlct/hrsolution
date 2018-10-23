effc85e58eb7430dc3e74d4841ce9d62dd32adef
Farzad Rahman <farzadrahman59@gmail.com>
2018-10-23 13:08:49 +0600

    Farzad

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-    public static get API_URL(): string { return "http://localhost/angular/hrsolution/backend/api/"; };
-    public static get Image_URL(): string { return "http://localhost/angular/hrsolution/backend/public/"; };
+    public static get API_URL(): string { return "http://192.168.3.95/angular/hrsolution/backend/api/"; };
+    public static get Image_URL(): string { return "http://192.168.3.95/angular/hrsolution/backend/public/"; };
 
 }
