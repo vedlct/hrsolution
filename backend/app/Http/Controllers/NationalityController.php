c64ff040b268041a9d8ae6f3cd56c07d6766425a
nsmajm <nsmajm@gmail.com>
2018-10-27 11:26:12 +0600

    nasim

--- backend/app/Http/Controllers/NationalityController.php
+++ backend/app/Http/Controllers/NationalityController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\country;
+use App\Country;
 
 class NationalityController extends Controller
 {
     public function getNationality(){
-        $nationality = country::select('code','Name')->get();
+        $nationality = Country::select('code','Name')->get();
        // return $nationality;
         return response()->json($nationality);
     }
