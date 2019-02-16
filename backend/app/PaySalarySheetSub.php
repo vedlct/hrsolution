bb360ec3e4e134d7a3ef9b78bd74c6878cf3596d
Arabi Kabir <mail2arabi@gmail.com>
2019-02-16 15:09:55 +0600

    arabi

---
+++ backend/app/PaySalarySheetSub.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PaySalarySheetSub extends Model
+{
+    protected $table = "paysalarysheetsub";
+    protected $primaryKey='id';
+    public $timestamps = false;
+}
