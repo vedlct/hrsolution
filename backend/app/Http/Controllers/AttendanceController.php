8b86d9d97c740559d60633914c906bb268d50b38
Farzad <farzadrahman59@gmail.com>
2019-03-12 12:40:33 +0600

    Farzad

--- backend/app/Http/Controllers/AttendanceController.php
+++ backend/app/Http/Controllers/AttendanceController.php
@@ -154,4 +154,134 @@ class AttendanceController extends Controller
 
 
     }
+
+
+
+    public function liveAttendance(){
+        //Live Attendance
+        $today=date('Y-m-d');
+
+
+        $morningTotal=EmployeeInfo::leftJoin('shiftlog','shiftlog.fkemployeeId','employeeinfo.id')
+            ->leftJoin('shift','shift.shiftId','shiftlog.fkshiftId')
+            ->where('employeeinfo.fkDepartmentId',6)
+            ->where('shiftlog.endDate',null)
+            ->whereIn('shiftlog.fkshiftId',[2,4])
+            ->count();
+
+
+        $eveningTotal=EmployeeInfo::leftJoin('shiftlog','shiftlog.fkemployeeId','employeeinfo.id')
+            ->leftJoin('shift','shift.shiftId','shiftlog.fkshiftId')
+            ->where('employeeinfo.fkDepartmentId',6)
+            ->where('shiftlog.endDate',null)
+            ->where('shiftlog.fkshiftId',3)
+            ->count();
+
+
+
+
+        $morningPresent = DB::select( DB::raw("select count(*) as present
+                from attendancedata ad left join attemployeemap em on ad.attDeviceUserId = em.attDeviceUserId
+                left join employeeinfo e on em.employeeId = e.id
+                left join shiftlog sl on e.id = sl.fkemployeeId 
+                where sl.endDate is null and (date_format(ad.accessTime,'%Y-%m-%d %H:%i:%s') between '".$today." 06:00:00' and '".$today." 14:59:59') and e.fkDepartmentId = 6 AND (sl.fkshiftId=2 OR sl.fkshiftId=4)
+                group by date_format(ad.accessTime,'%Y-%m-%d')"));
+
+
+
+
+
+        $eveningPresent = DB::select( DB::raw("select count(*) as present
+                from attendancedata ad left join attemployeemap em on ad.attDeviceUserId = em.attDeviceUserId
+                left join employeeinfo e on em.employeeId = e.id
+                left join shiftlog sl on e.id = sl.fkemployeeId 
+                where sl.endDate is null and (date_format(ad.accessTime,'%Y-%m-%d %H:%i:%s') between '".$today." 14:00:00' and '".$today." 22:59:59') and e.fkDepartmentId = 6 AND sl.fkshiftId=3
+                group by date_format(ad.accessTime,'%Y-%m-%d')"));
+
+
+
+
+
+
+
+
+
+
+        $morningLate = DB::select( DB::raw("select count(*) as LateTotal from
+            (select ad.id,ad.attDeviceUserId, em.employeeId, e.firstName
+            , date_format(ad.accessTime,'%Y-%m-%d %h:%i:%s') attendanceDate
+            , date_format(min(ad.accessTime),'%h:%i:%s %p') checkIn
+            , date_format(max(ad.accessTime),'%h:%i:%s %p') checkOut
+            , case when SUBTIME(date_format(min(ad.accessTime),'%H:%i'),s.inTime) > '00:00:01' then 'Y' else 'N' end late
+            
+            from attendancedata ad left join attemployeemap em on ad.attDeviceUserId = em.attDeviceUserId
+            left join employeeinfo e on em.employeeId = e.id
+            left join shiftlog sl on em.employeeId = sl.fkemployeeId and date_format(ad.accessTime,'%Y-%m-%d') between date_format(sl.startDate,'%Y-%m-%d') and ifnull(date_format(sl.endDate,'%Y-%m-%d'),curdate())
+            left join shift s on sl.fkshiftId = s.shiftId
+            where (date_format(ad.accessTime,'%Y-%m-%d %H:%i:%s') between '".$today." 07:02:00' and '".$today." 13:59:59') and e.fkDepartmentId = 6
+            group by em.employeeId, date_format(ad.accessTime,'%Y-%m-%d')) a
+            where a.late = 'Y'"));
+
+
+
+
+
+
+
+
+
+        $eveningLate = DB::select( DB::raw("select count(*) as LateTotal from
+            (select ad.id,ad.attDeviceUserId, em.employeeId, e.firstName
+            , date_format(ad.accessTime,'%Y-%m-%d %h:%i:%s') attendanceDate
+            , date_format(min(ad.accessTime),'%h:%i:%s %p') checkIn
+            , date_format(max(ad.accessTime),'%h:%i:%s %p') checkOut
+            , case when SUBTIME(date_format(min(ad.accessTime),'%H:%i'),s.inTime) > '00:00:01' then 'Y' else 'N' end late
+            
+            from attendancedata ad left join attemployeemap em on ad.attDeviceUserId = em.attDeviceUserId
+            left join employeeinfo e on em.employeeId = e.id
+            left join shiftlog sl on em.employeeId = sl.fkemployeeId and date_format(ad.accessTime,'%Y-%m-%d') between date_format(sl.startDate,'%Y-%m-%d') and ifnull(date_format(sl.endDate,'%Y-%m-%d'),curdate())
+            left join shift s on sl.fkshiftId = s.shiftId
+            where (date_format(ad.accessTime,'%Y-%m-%d %H:%i:%s') between '".$today." 15:02:00' and '".$today." 22:59:59') and e.fkDepartmentId = 6
+            group by em.employeeId, date_format(ad.accessTime,'%Y-%m-%d')) a
+            where a.late = 'Y'"));
+
+
+
+
+        if($morningPresent){
+            $morningPresent= $morningPresent[0];
+        }
+        else{
+            $morningPresent=0;
+        }
+        if($eveningPresent){
+            $eveningPresent= $eveningPresent[0];
+        }
+        else{
+            $eveningPresent=0;
+        }
+        if($morningLate){
+            $morningLate= $morningLate[0]->LateTotal;
+        }
+        else{
+            $morningLate=0;
+        }
+
+
+        if($eveningLate){
+            $eveningLate= $eveningLate[0]->LateTotal;
+        }
+        else{
+            $eveningLate=0;
+        }
+
+
+
+    return response()->json(['morningTotal'=>$morningTotal,'morningPresent'=>$morningPresent,'morningLate'=>$morningLate,
+                    'eveningTotal'=>$eveningTotal,'eveningPresent'=>$eveningPresent,'eveningLate'=>$eveningLate]);
+
+
+
+
+    }
 }
