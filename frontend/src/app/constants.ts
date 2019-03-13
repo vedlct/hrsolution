76cc734386d307f5b25d33de57247a45c16923b7
Arabi Kabir <mail2arabi@gmail.com>
2019-03-13 10:51:32 +0600

    arabi

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -1,8 +1,8 @@
 export class Constants {
 
 
-    public static get API_URL(): string { return "http://192.168.3.95:8000/hrsolution/backend/api/"; };
-    public static get Image_URL(): string { return "http://192.168.3.95:8000/hrsolution/backend/public/"; };
+    public static get API_URL(): string { return "http://192.168.3.89/hrsolution/backend/api/"; };
+    public static get Image_URL(): string { return "http://192.168.3.89/hrsolution/backend/public/"; };
 
 
 
