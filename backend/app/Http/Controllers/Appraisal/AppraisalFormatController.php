af3aeac21bd927b42233cabd42665a17653b2662
Farzad <farzadrahman59@gmail.com>
2019-06-27 13:23:26 +0600

    Farzad

--- backend/app/Http/Controllers/Appraisal/AppraisalFormatController.php
+++ backend/app/Http/Controllers/Appraisal/AppraisalFormatController.php
@@ -78,8 +78,8 @@ class AppraisalFormatController extends Controller
     }
 
     public function showAllHeadsAppraisalFormate(){
-        $appraisalGroups=AppraisalHead::select('appraisalheads.headName','appraisalheads.id')
-            ->where('appraisalheads.fk_Appraisalheads',null)
+        $appraisalGroups=AppraisalHead::select('appraisalheads.headName','appraisalheads.id','headType')
+            ->where('appraisalheads.fk_Appraisalheads','!=',null)
             ->get();
 
         $appraisalGroups->map(function ($post) {
