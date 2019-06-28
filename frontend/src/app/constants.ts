be100b5e6a9120eb868e1646c7fc9da8a5dfea53
sakib <md.sakibrahman@gmail.com>
2019-06-28 12:06:02 +0600

    sakib

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -2,8 +2,8 @@ export class Constants {
 
 
 
-    public static get API_URL(): string { return "http://localhost/hrsolution/backend/api/"; };
-    public static get Image_URL(): string { return "http://localhost/hrsolution/backend/public/"; };
+    public static get API_URL(): string { return "http://192.168.3.20/hrsolution/backend/api/"; };
+    public static get Image_URL(): string { return "http://192.168.3.20/hrsolution/backend/public/"; };
 
 
 }
