63102e6045e612349a2405d616d628349b5da931
Farzad <farzadrahman59@gmail.com>
2019-10-04 13:45:11 +0600

    Farzad

--- frontend/src/app/components/appraisal/create-appraisal-employee/create-appraisal-employee.component.ts
+++ frontend/src/app/components/appraisal/create-appraisal-employee/create-appraisal-employee.component.ts
@@ -1,54 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {TokenService} from "../../../services/token.service";
 import {Constants} from "../../../constants";
 import {IDropdownSettings} from "ng-multiselect-dropdown/multiselect.model";
 
 @Component({
-  selector: 'app-create-appraisal-employee',
-  templateUrl: './create-appraisal-employee.component.html',
-  styleUrls: ['./create-appraisal-employee.component.css']
+    selector: 'app-create-appraisal-employee',
+    templateUrl: './create-appraisal-employee.component.html',
+    styleUrls: ['./create-appraisal-employee.component.css']
 })
 export class CreateAppraisalEmployeeComponent implements OnInit {
-    allEmployees:any;
-    dropdownList :any= [];
+    allEmployees: any;
+    allDept: any;
+    dropdownList: any = [];
     selectedItems = [];
     dropdownSettings = {};
-  constructor(private http:HttpClient,private token:TokenService) { }
+    appraisers:any=[];
 
-  ngOnInit() {
+    constructor(private http: HttpClient, private token: TokenService) {
+    }
+
+    ngOnInit() {
+        this.getAllEmp();
+        this.getAllDept();
 
 
-      const token=this.token.get();
-      this.http.get(Constants.API_URL+'employee/basicinfo/all'+'?token='+token).subscribe(data => {
-               console.log(data);
-               this.allEmployees=data;
-              this.dropdownList=data;
+    }
 
-              this.dropdownSettings = {
-                  singleSelection: false,
-                  idField: 'empid',
-                  textField: 'ConcatenatedString',
-                  selectAllText: 'Select All',
-                  unSelectAllText: 'UnSelect All',
-                  itemsShowLimit: 3,
-                  allowSearchFilter: true
-              };
+    getAllEmp() {
+        const token = this.token.get();
+        this.http.get(Constants.API_URL + 'employee/basicinfo/all' + '?token=' + token).subscribe(data => {
+                console.log(data);
+                this.allEmployees = data;
+                this.dropdownList = data;
 
-          },
-          error => {
-              console.log(error.error['error']);
+                this.dropdownSettings = {
+                    singleSelection: false,
+                    idField: 'empid',
+                    textField: 'ConcatenatedString',
+                    selectAllText: 'Select All',
+                    unSelectAllText: 'UnSelect All',
+                    itemsShowLimit: 3,
+                    allowSearchFilter: true
+                };
 
-          }
-      );
-  }
+            },
+            error => {
+                console.log(error.error['error']);
 
+            }
+        );
+    }
+
+    getAllDept(){
+        // department/get
+        const token = this.token.get();
+        this.http.get(Constants.API_URL + 'department/get' + '?token=' + token).subscribe(data => {
+                console.log(data);
+                this.allDept=data;
+
+
+            },
+            error => {
+                console.log(error.error['error']);
+
+            }
+        );
+    }
 
     onItemSelect(item: any) {
         console.log(item);
     }
+
     onSelectAll(items: any) {
         console.log(items);
     }
 
+    onDeptChange(val){
+        console.log(val);
+    }
+
+    addRole(){
+        this.appraisers.push(this.appraisers.length);
+
+    }
+
+    subRole(){
+        this.appraisers.pop();
+    }
 }
